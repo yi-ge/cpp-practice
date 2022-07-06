0c19def9112d4556c42c892d4f71d844e91ac636
yi-ge <a@wyr.me>
2022-07-06 22:17:17 +0800

    feat: Code coverage.

--- bin/lcov2json.js
+++ bin/lcov2json.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
 const parse = require('lcov-parse')
 const path = require('path')
+const fs = require('fs')
 
 parse(path.join(__dirname, '../coverage.info'), function(err, data) {
     //process the data here
-    console.log(data)
+    // console.log(data)
+    const res = {
+      conclusion: err ? 'failure' : (data?.[0]?.functions?.hit === data?.[0]?.functions?.found ? 'success' : 'neutral'),
+      stats: {
+        tests: data?.[0]?.functions?.found,
+        runs: data?.[0]?.functions?.hit
+      },
+      data
+    }
+    // fs.writeFileSync(path.join(__dirname, '../outputs.json'), JSON.stringify(res, null, 2), 'utf-8')
+    console.log(`::set-output name=outputs.json::${JSON.stringify(res)}`)
 });
--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+COVERAGE_FOLDER=coverage
+COVERAGE_FILE=lcov.info
+REPORT_FOLDER=report
 
-COVERAGE_FILE=coverage.info
-REPORT_FOLDER=coverage_report
-PWD=`pwd`
-lcov --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FILE}_tmp
-lcov --remove ${COVERAGE_FILE}_tmp -o ${COVERAGE_FILE}_tmp "*/include/*"
-lcov --rc lcov_branch_coverage=1 -e ${COVERAGE_FILE}_tmp "*src*" -o ${COVERAGE_FILE}
-genhtml --rc genhtml_branch_coverage=1 ${COVERAGE_FILE} -o ${REPORT_FOLDER}
+mkdir ${COVERAGE_FOLDER}
+lcov --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
+lcov --remove ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp "*/include/*"
+lcov --rc lcov_branch_coverage=1 -e ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp "*src*" -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}
+genhtml --rc genhtml_branch_coverage=1 ${COVERAGE_FOLDER}/${COVERAGE_FILE} -o ${COVERAGE_FOLDER}/${REPORT_FOLDER}
