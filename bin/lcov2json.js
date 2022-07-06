7fac13bf91fb8d4c62e47fd8ebf545d55f838bbb
yi-ge <a@wyr.me>
2022-07-06 22:57:56 +0800

    fix: node.js script.

--- bin/lcov2json.js
+++ bin/lcov2json.js
@@ -1,11 +1,8 @@
-#!/usr/bin/env node
 const parse = require('lcov-parse')
 const path = require('path')
 const fs = require('fs')
 
 parse(path.join(__dirname, '../coverage.info'), function(err, data) {
-    //process the data here
-    // console.log(data)
     const res = {
       conclusion: err ? 'failure' : (data?.[0]?.functions?.hit === data?.[0]?.functions?.found ? 'success' : 'neutral'),
       stats: {
