d181a17f92e0f145618097b1d5d7527046d8821f
yi-ge <a@wyr.me>
2022-07-08 20:36:44 +0800

    chore: use llvm 14

--- bin/ci.cjs
+++ bin/ci.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 const bashStr = `#!/bin/bash
-exec llvm-cov gcov "$@"
+exec llvm-cov-14 gcov "$@"
 `
 
 fs.writeFileSync('/usr/bin/llvm-gcov.sh', bashStr, 'utf-8')
--- coverage.sh
+++ coverage.sh
@@ -3,8 +3,23 @@ COVERAGE_FOLDER=coverage
 COVERAGE_FILE=lcov.info
 REPORT_FOLDER=report
 
+rm -rf ${COVERAGE_FOLDER}
 mkdir ${COVERAGE_FOLDER}
-lcov --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
+
+uNames=`uname -s`
+osName=${uNames: 0: 4}
+if [ "$osName" == "Darw" ] # Darwin
+then
+	lcov --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
+elif [ "$osName" == "Linu" ] # Linux
+then
+	lcov --gcov-tool llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
+elif [ "$osName" == "MING" ] # MINGW, windows, git-bash
+then
+  lcov --gcov-tool llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
+fi
+
+lcov --gcov-tool llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
 lcov --remove ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp "*/include/*"
 lcov --rc lcov_branch_coverage=1 -e ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp "*src*" -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}
 genhtml --rc genhtml_branch_coverage=1 ${COVERAGE_FOLDER}/${COVERAGE_FILE} -o ${COVERAGE_FOLDER}/${REPORT_FOLDER}
