9dd1c56a155d4fd12f7c2b0288d03795ba28980d
yi-ge <a@wyr.me>
2022-08-23 22:42:27 +0800

    add: 分数加减运算

--- src/math/fraction_addition_and_subtraction.cpp
+++ src/math/fraction_addition_and_subtraction.cpp
@@ -1,49 +1,45 @@
 // 分数加减运算
 // https://leetcode.cn/problems/fraction-addition-and-subtraction
 // INLINE  ../images/math/fraction_addition_and_subtraction.jpeg
+// 解题思路：参考官方题解，此题我也是参考题解的。
 #include <headers.hpp>
+#include <numeric>
 
 class Solution
 {
 public:
   string fractionAddition(string expression)
   {
-    long long numerator = 0;   // 分子
-    long long denominator = 1; // 分母
+    long long x = 0, y = 1; // 分子，分母
     int index = 0, n = expression.size();
-    while (index < n)
-    {
-      // 读取分子
-      long long denominator1 = 0, sign = 1;
-      if (expression[index] == '-' || expression[index] == '+')
-      {
-        sign = expression[index] == '-' ? -1 : 1;
+    while (index < n) {
+        // 读取分子
+        long long x1 = 0, sign = 1;
+        if (expression[index] == '-' || expression[index] == '+') {
+            sign = expression[index] == '-' ? -1 : 1;
+            index++;
+        }
+        while (index < n && isdigit(expression[index])) {
+            x1 = x1 * 10 + expression[index] - '0';
+            index++;
+        }
+        x1 = sign * x1;
         index++;
-      }
-      while (index < n && isdigit(expression[index]))
-      {
-        denominator1 = denominator1 * 10 + expression[index] - '0';
-        index++;
-      }
-      denominator1 = sign * denominator1;
-      index++;
 
-      // 读取分母
-      long long numerator1 = 0;
-      while (index < n && isdigit(expression[index]))
-      {
-        numerator1 = numerator1 * 10 + expression[index] - '0';
-        index++;
-      }
+        // 读取分母
+        long long y1 = 0;
+        while (index < n && isdigit(expression[index])) {
+            y1 = y1 * 10 + expression[index] - '0';
+            index++;
+        }
 
-      denominator = denominator * numerator1 + denominator1 * numerator;
-      numerator *= numerator1;
+        x = x * y1 + x1 * y;
+        y *= y1;
     }
-    if (denominator == 0)
-    {
-      return "0/1";
+    if (x == 0) {
+        return "0/1";
     }
-    long long g = gcd(abs(denominator), numerator); // 获取最大公约数
-    return to_string(denominator / g) + "/" + to_string(numerator / g);
+    long long g = gcd(abs(x), y); // 获取最大公约数，C17+ 需要引入 numeric 库
+    return to_string(x / g) + "/" + to_string(y / g);
   }
 };
--- test/math/fraction_addition_and_subtraction_test.cpp
+++ test/math/fraction_addition_and_subtraction_test.cpp
@@ -11,10 +11,10 @@ TEST(分数加减运算, fractionAddition)
   //  示例 2:
   // 输入: expression = "-1/2+1/2+1/3"
   // 输出: "1/3"
-  EXPECT_EQ(solution.fractionAddition("1/2+1/2+1/3").c_str(), "1/3");
+  EXPECT_STREQ(solution.fractionAddition("-1/2+1/2+1/3").c_str(), "1/3");
 
   // 示例 3:
   // 输入: expression = "1/3-1/2"
   // 输出: "-1/6"
-  EXPECT_EQ(solution.fractionAddition("1/3-1/2").c_str(), "-1/6");
+  EXPECT_STREQ(solution.fractionAddition("1/3-1/2").c_str(), "-1/6");
 }
