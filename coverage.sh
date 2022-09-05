18fc725dff6fd9d3fd2d49b763ac7ad055a3743d
yi-ge <a@wyr.me>
2022-09-05 16:35:30 +0800

    fix: 优化本地代码覆盖率测试配置

--- coverage.sh
+++ coverage.sh
@@ -22,4 +22,4 @@ fi
 lcov --gcov-tool llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
 lcov --remove ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp "*/include/*"
 lcov --rc lcov_branch_coverage=1 -e ${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp "*src*" -o ${COVERAGE_FOLDER}/${COVERAGE_FILE}
-genhtml --rc genhtml_branch_coverage=1 ${COVERAGE_FOLDER}/${COVERAGE_FILE} -o ${COVERAGE_FOLDER}/${REPORT_FOLDER}
+# genhtml --rc genhtml_branch_coverage=1 ${COVERAGE_FOLDER}/${COVERAGE_FILE} -o ${COVERAGE_FOLDER}/${REPORT_FOLDER}
