839c93bd4e8134cbc1c615bbd22c94c194810c0c
yi-ge <a@wyr.me>
2022-09-07 14:36:40 +0800

    fix: 调整覆盖率测试shell

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,20 +6,17 @@ REPORT_FOLDER=report
 rm -rf ${COVERAGE_FOLDER}
 mkdir ${COVERAGE_FOLDER}
 
-uNames=`uname -s`
-osName=${uNames: 0: 4}
+uNames=$(uname -s)
+osName=${uNames:0:4}
 
 TMP_COVERAGE_FILE=${COVERAGE_FOLDER}/${COVERAGE_FILE}_tmp
 
-if [ "$osName" == "Darw" ] # Darwin
-then
-	lcov --gcov-tool $PWD/scripts/llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${TMP_COVERAGE_FILE}
-elif [ "$osName" == "Linu" ] # Linux
-then
-	lcov --gcov-tool llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${TMP_COVERAGE_FILE}
-elif [ "$osName" == "MING" ] # MINGW, windows, git-bash
-then
-  lcov --gcov-tool llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${TMP_COVERAGE_FILE}
+if [ "$osName" == "Darw" ]; then # Darwin
+  lcov --gcov-tool $PWD/scripts/llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${TMP_COVERAGE_FILE}
+elif [ "$osName" == "Linu" ]; then # Linux
+  lcov --gcov-tool $PWD/scripts/llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${TMP_COVERAGE_FILE}
+elif [ "$osName" == "MING" ]; then # MINGW, windows, git-bash
+  lcov --gcov-tool $PWD/scripts/llvm-gcov.sh --rc lcov_branch_coverage=1 -c -d build -o ${TMP_COVERAGE_FILE}
 fi
 
 lcov --remove ${TMP_COVERAGE_FILE} -o ${TMP_COVERAGE_FILE} "*/include/*"
