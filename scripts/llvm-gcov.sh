6e1feed9ead5a5ae61cf2114ea5d4ea427911bee
yi-ge <a@wyr.me>
2022-09-07 14:43:41 +0800

    fix: 调整覆盖率测试shell

--- scripts/llvm-gcov.sh
+++ scripts/llvm-gcov.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
-uNames=`uname -s`
-osName=${uNames: 0: 4}
+#!/bin/bash
+uNames=$(uname -s)
+osName=${uNames:0:4}
 
-if [ "$osName" == "Darw" ] # Darwin
-then
-	exec llvm-cov gcov "$@"
-elif [ "$osName" == "Linu" ] # Linux
-then
-	exec llvm-cov-15 gcov "$@"
-elif [ "$osName" == "MING" ] # MINGW, windows, git-bash
-then
+if [ "$osName" == "Darw" ]; then # Darwin
+  exec llvm-cov gcov "$@"
+elif [ "$osName" == "Linu" ]; then # Linux
+  exec llvm-cov-15 gcov "$@"
+elif [ "$osName" == "MING" ]; then # MINGW, windows, git-bash
   exec llvm-cov-15 gcov "$@"
 fi
