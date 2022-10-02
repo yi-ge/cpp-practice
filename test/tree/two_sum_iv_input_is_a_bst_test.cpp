d4d34bf7a1930ea154ef0f69bea2d4fc2aee3b4f
yi-ge <a@wyr.me>
2022-10-03 00:26:12 +0800

    fix: 两数之和IV名称导致测试异常

--- test/tree/two_sum_iv_input_is_a_bst_test.cpp
+++ test/tree/two_sum_iv_input_is_a_bst_test.cpp
@@ -1,7 +1,7 @@
 #include "tree_node.hpp"
 #include <tree/two_sum_iv_input_is_a_bst.cpp>
 
-TEST(两数之和_IV, TreeNode) {
+TEST(两数之和四, TreeNode) {
   // 示例 1：
   // 输入: root = [5,3,6,2,4,null,7], k = 9
   // 输出: true
