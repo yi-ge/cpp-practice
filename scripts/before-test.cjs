f476c669d76591eec3ed461f5f6421594d1d1177
yi-ge <a@wyr.me>
2023-05-26 18:31:12 +0800

    to: 测试前脚本

--- scripts/before-test.cjs
+++ scripts/before-test.cjs
@@ -1,4 +1,3 @@
-// const { execSync } = require("child_process")
 const fs = require('fs')
 const path = require('path')
 
@@ -7,16 +6,3 @@ const filePath = path.join(process.cwd(), 'build', 'default.profraw')
 if (fs.existsSync(filePath)) {
   fs.unlinkSync(filePath)
 }
-
-// 删除已存在的.gcda文件
-// // find ${CMAKE_BINARY_DIR} -name \*.gcda -delete
-// const CMAKE_BINARY_DIR = path.join(process.cwd(), 'build')
-// const out = execSync("find", [CMAKE_BINARY_DIR, "-name", "*.gcda", "-delete"], {
-//   cwd: path.join(process.cwd()), // 当前工作目录
-//   env: process.env,  // 附加当前操作系统的环境变量
-//   stdio: 'inherit',  // 混合标准输出和错误输出
-// })
-// console.log(out.toString())
-
-// 删除与当前单元测试无关的.gcda文件
-// # find build -name "*.gcda" -not -path "*flip_columns_for_maximum_number_of_equal_rows*" -exec rm {} \;
