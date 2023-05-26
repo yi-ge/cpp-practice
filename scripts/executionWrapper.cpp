4fa4d48994fe67e880a3f1fbb67abd0a8bfca2e1
yi-ge <a@wyr.me>
2023-05-26 16:57:06 +0800

    to: 适配Windows平台带颜色输出

--- scripts/executionWrapper.cpp
+++ scripts/executionWrapper.cpp
@@ -5,6 +5,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <deque>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -13,7 +14,7 @@
 
 namespace fs = std::filesystem;
 
-std::string exec_and_capture(const std::string &cmd) {
+std::string exec_and_capture(const std::string &cmd, bool keep_color) {
   std::array<char, 128> buffer;
   std::string result;
   std::unique_ptr<FILE, decltype(&_pclose)> pipe(
@@ -24,7 +25,31 @@ std::string exec_and_capture(const std::string &cmd) {
   while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
     result += buffer.data();
   }
+  if (!keep_color) {
+    // Remove ANSI color codes
+    result =
+        std::regex_replace(result, std::regex("\x1B\\[[0-9;]*[a-zA-Z]"), "");
+  }
   return result;
+}
+
+void trim_file(const fs::path &path, std::size_t max_lines) {
+  std::ifstream in(path);
+  std::deque<std::string> lines;
+  std::string line;
+  while (std::getline(in, line)) {
+    lines.push_back(line);
+    if (lines.size() > max_lines) {
+      lines.pop_front();
+    }
+  }
+  in.close();
+
+  std::ofstream out(path);
+  for (const auto &l : lines) {
+    out << l << '\n';
+  }
+  out.close();
 }
 
 int main(int argc, char *argv[]) {
@@ -40,10 +65,8 @@ int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) {
     if (std::string(argv[i]) == "-c" && i + 1 < argc) {
       command = argv[++i];
-      // 在此处移除命令及其参数的外部引号（如有），注意，仅适用于没有空格的文件名和路径，否则仍然需要用双引号来处理带有空格的路径。
       command.erase(std::remove(command.begin(), command.end(), '\"'),
                     command.end());
-      // Replace -gtest_color=no with -gtest_color=yes
       if (command.find("-gtest_color=no") != std::string::npos) {
         command = std::regex_replace(command, std::regex("-gtest_color=no"),
                                      "-gtest_color=yes");
@@ -61,22 +84,27 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  std::string output = exec_and_capture(command);
-  fs::path logPath = logDir / logFile;
-  fs::create_directories(logPath.parent_path());
-  std::ofstream outFile(logPath, std::ios_base::out | std::ios_base::app);
-  if (outFile.is_open()) {
-    outFile << output;
-    outFile.close();
-  } else {
-    std::cerr << "Unable to open the output file." << std::endl;
-    return 1;
-  }
+  // Write command to command.txt
+  fs::path cmdPath = logDir / "command.txt";
+  std::ofstream cmdFile(cmdPath, std::ios::app);
+  std::time_t currentTime = std::time(nullptr);
+  cmdFile << std::asctime(std::localtime(&currentTime))
+          << "Command: " << command << "\n";
+  cmdFile.close();
+  trim_file(cmdPath, 2000);
 
-  // ANSI escape codes start with '\x1B'
-  std::regex ansi_regex("\x1B\\[[0-9;]*[mK]");
-  std::string stripped_output = std::regex_replace(output, ansi_regex, "");
-  std::cout << stripped_output;
+  // Execute command and capture output:
+  std::string output = exec_and_capture(command, logFile == "gtest.log");
+
+  // Write the output to file:
+  fs::path outputPath = logDir / logFile;
+  std::ofstream logFile(outputPath, std::ios::app);
+  logFile << output;
+  logFile.close();
+  trim_file(outputPath, 5000);
+
+  // Print the output to the console (without color codes):
+  std::cout << exec_and_capture(command, false);
 
   return 0;
 }
