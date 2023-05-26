7fe3046e5e57ad410054747df07ff08497353192
yi-ge <a@wyr.me>
2023-05-26 08:33:56 +0800

    feat: 适配linux平台

--- scripts/coverage.cjs
+++ scripts/coverage.cjs
@@ -1,5 +1,6 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
+const fs = require('fs')
 
 // 在 Windows 上，二进制文件的扩展名是 .exe
 // 在其他系统上，扩展名是空字符串
@@ -9,16 +10,22 @@ const extension = process.platform === 'win32' ? '.exe' : '';
 const output = `grcov${extension}`;
 const filePath = path.join(process.cwd(), 'scripts', output);
 
+// 如果coverage目录不存在，则创建
+const dir = path.join(process.cwd(), 'coverage')
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+}
+
 // 执行覆盖率文件生成命令
-let command = '' // 如果参数中有lcov，则生成 lcov.info 文件，否则生成 html 文件
+let command = filePath // 如果参数中有lcov，则生成 lcov.info 文件，否则生成 html 文件
+let args = ''
 if (process.argv.includes('lcov')) {
-  command = filePath + ' . -s . --binary-path ./build/ -t lcov --llvm --branch --guess-directory-when-missing --ignore test/\* -o ./coverage/lcov.info'
+  args = ' . -s . --binary-path ./build/ -t lcov --llvm --branch --guess-directory-when-missing --ignore include/* --ignore build/* --ignore test/* -o ./coverage/lcov.info'
 } else {
-  command = filePath + ' . -s . --binary-path ./build/ -t html --llvm --branch --guess-directory-when-missing --ignore test/\* -o ./coverage/'
+  args = ' . -s . --binary-path ./build/ -t html --llvm --branch --guess-directory-when-missing --ignore include/* --ignore build/* --ignore test/* -o ./coverage/'
 }
 console.log(command)
-
-const execute = spawnSync(command, {
+const execute = spawnSync(command, args.split(' '), {
   cwd: path.join(process.cwd()), // 当前工作目录
   env: process.env,  // 附加当前操作系统的环境变量
   stdio: 'inherit',  // 混合标准输出和错误输出
