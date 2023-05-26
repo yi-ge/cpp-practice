87fc6c6fe239a367989b37db1fe8491079bd06e4
yi-ge <a@wyr.me>
2023-05-26 18:30:12 +0800

    test: 测试Windows平台带颜色输出

--- scripts/executionWrapper.cpp
+++ scripts/executionWrapper.cpp
@@ -2,109 +2,114 @@
 // clang++ -std=c++17 -o executionWrapper.exe executionWrapper.cpp
 #include <algorithm>
 #include <array>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
+#include <chrono>
 #include <deque>
-#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <regex>
+#include <sstream>
 #include <string>
+#include <vector>
 
-namespace fs = std::filesystem;
+// Method to parse the command line arguments
+void parse_arguments(int argc, char **argv, std::string &command,
+                     std::string &logDir) {
+  for (int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+    if ((arg == "-c") && (i + 1 < argc)) {
+      command = argv[++i];
+    } else if ((arg == "-l") && (i + 1 < argc)) {
+      logDir = argv[++i];
+    } else {
+      std::cerr << "Unknown option: " << arg << std::endl;
+      exit(1);
+    }
+  }
+}
 
-std::string exec_and_capture(const std::string &cmd, bool keep_color) {
+// Function to execute a command and capture the output
+std::string exec_and_capture(const char *cmd) {
   std::array<char, 128> buffer;
   std::string result;
-  std::unique_ptr<FILE, decltype(&_pclose)> pipe(
-      _popen((cmd + " 2>&1").c_str(), "r"), _pclose);
+  std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
   if (!pipe) {
     throw std::runtime_error("popen() failed!");
   }
   while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
     result += buffer.data();
   }
-  if (!keep_color) {
-    // Remove ANSI color codes
-    result =
-        std::regex_replace(result, std::regex("\x1B\\[[0-9;]*[a-zA-Z]"), "");
-  }
   return result;
 }
 
-void trim_file(const fs::path &path, std::size_t max_lines) {
-  std::ifstream in(path);
+// Limit the lines of a file
+void limit_file_lines(const std::string &filePath, int maxLines) {
+  std::ifstream file(filePath);
   std::deque<std::string> lines;
   std::string line;
-  while (std::getline(in, line)) {
+  while (std::getline(file, line)) {
     lines.push_back(line);
-    if (lines.size() > max_lines) {
+    while (lines.size() > maxLines) {
       lines.pop_front();
     }
   }
-  in.close();
+  file.close();
 
-  std::ofstream out(path);
-  for (const auto &l : lines) {
-    out << l << '\n';
+  std::ofstream fileOut(filePath);
+  for (const auto &line : lines) {
+    fileOut << line << std::endl;
   }
-  out.close();
+  fileOut.close();
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " -c <command> -l <log path>"
-              << std::endl;
-    return 1;
-  }
-
+int main(int argc, char **argv) {
   std::string command;
-  fs::path logDir;
-  std::string logFile;
-  for (int i = 1; i < argc; i++) {
-    if (std::string(argv[i]) == "-c" && i + 1 < argc) {
-      command = argv[++i];
-      command.erase(std::remove(command.begin(), command.end(), '\"'),
-                    command.end());
-      if (command.find("-gtest_color=no") != std::string::npos) {
-        command = std::regex_replace(command, std::regex("-gtest_color=no"),
-                                     "-gtest_color=yes");
-        logFile = "gtest.log";
-      } else {
-        logFile = "output.log";
-      }
-    } else if (std::string(argv[i]) == "-l" && i + 1 < argc) {
-      logDir = fs::path(argv[++i]);
-    }
-  }
+  std::string logDir;
 
-  if (command.empty() || logDir.empty()) {
-    std::cerr << "Both command and log directory are required." << std::endl;
-    return 1;
-  }
+  parse_arguments(argc, argv, command, logDir);
 
-  // Write command to command.txt
-  fs::path cmdPath = logDir / "command.txt";
-  std::ofstream cmdFile(cmdPath, std::ios::app);
-  std::time_t currentTime = std::time(nullptr);
-  cmdFile << std::asctime(std::localtime(&currentTime))
-          << "Command: " << command << "\n";
+  // Remove command and its arguments external quotes, if any
+  command.erase(std::remove(command.begin(), command.end(), '\"'),
+                command.end());
+
+  // Construct powershell command
+  std::string powershellCmd =
+      "powershell.exe -Command \"& { " + command + " }\"";
+
+  // Get the current date and time
+  auto currentTime = std::chrono::system_clock::now();
+  std::time_t currentTimeT = std::chrono::system_clock::to_time_t(currentTime);
+  std::string currentTimeStr(std::ctime(&currentTimeT));
+  currentTimeStr.erase(currentTimeStr.end() - 1); // Remove trailing newline
+
+  // Write current time and command to command.txt
+  std::ofstream cmdFile(logDir + "/command.txt", std::ios_base::app);
+  cmdFile << "Time: " << currentTimeStr << ", Command: " << command
+          << std::endl;
   cmdFile.close();
-  trim_file(cmdPath, 2000);
 
-  // Execute command and capture output:
-  std::string output = exec_and_capture(command, logFile == "gtest.log");
+  // Execute the command and capture the output with colors
+  std::string output = exec_and_capture(powershellCmd.c_str());
 
-  // Write the output to file:
-  fs::path outputPath = logDir / logFile;
-  std::ofstream logFile(outputPath, std::ios::app);
+  // Write output to the appropriate log file
+  std::string logPath = logDir + "/";
+  if (command.find("-gtest_color=no") != std::string::npos) {
+    logPath += "gtest.log";
+  } else {
+    logPath += "output.log";
+  }
+  std::ofstream logFile(logPath, std::ios_base::trunc);
   logFile << output;
   logFile.close();
-  trim_file(outputPath, 5000);
 
-  // Print the output to the console (without color codes):
-  std::cout << exec_and_capture(command, false);
+  // Limit the lines of the files
+  limit_file_lines(logDir + "/command.txt", 2000);
+  if (command.find("-gtest_color=no") == std::string::npos) {
+    limit_file_lines(logPath, 5000);
+  }
+
+  // Display the output to the screen without color
+  std::cout << std::regex_replace(output,
+                                  std::regex("\\x1B\\[[0-?]*[ -/]*[@-~]"), "");
 
   return 0;
 }
