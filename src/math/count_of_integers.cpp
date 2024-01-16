e404d0f0024186cc634c1f18b200ec4fd26170ec
yi-ge <a@wyr.me>
2024-01-16 09:32:55 +0800

    fix: 统计整数数目

--- src/math/count_of_integers.cpp
+++ src/math/count_of_integers.cpp
@@ -17,23 +17,23 @@ public:
     for (char c : num1)
       down_limit.push_back(c - '0');
 
-    std::function<int(int, int, bool, bool, bool)> f;
+    std::function<int(int, long long, bool, bool, bool)> f;
     std::unordered_map<long long, int> memo;
 
-    f = [&](int i, int s, bool valid, bool dlimit, bool ulimit) -> int {
+    f = [&](int i, long long s, bool valid, bool dlimit, bool ulimit) -> int {
       if (i == m) {
         return valid && min_sum <= s && s <= max_sum ? 1 : 0;
       }
       long long key =
-          i + 71LL * (s + 71LL * (valid + 2LL * (dlimit + 2LL * ulimit)));
+          i + 1001LL * (s + 1001LL * (valid + 2LL * (dlimit + 2LL * ulimit)));
       if (memo.count(key))
         return memo[key];
       int down = dlimit ? down_limit[i] : 0;
       int up = ulimit ? up_limit[i] : 9;
       int ans = 0;
       for (int d = down; d <= up; ++d) {
-        ans = (ans + f(i + 1, s + d, valid || d != 0, dlimit && d == down,
-                       ulimit && d == up)) %
+        ans = (ans + f(i + 1, (s + d) % mod, valid || d != 0,
+                       dlimit && d == down, ulimit && d == up)) %
               mod;
       }
       return memo[key] = ans;
